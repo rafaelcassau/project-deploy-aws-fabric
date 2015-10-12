a5db041315c9f4f97a84b98daf6ea1de85782894
Rafael <rafa.cassau@gmail.com>
2015-10-12 18:52:25 -0300

    add new features

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,11 @@
-from fabric.api import local, cd, run, env, sudo
+#encoding: utf-8
+
+from fabric.api import local, cd, run, env, prefix
 import os
 
 env.hosts = ['ec2-52-89-60-124.us-west-2.compute.amazonaws.com']
 env.user = 'ubuntu'
-env.key_filename = '~/.ssh/rafaelcassau.pem'
+env.key_filename = '~/rafaelcassau.pem'
 
 def runserver():
 	local("./manage.py runserver 8000")
@@ -23,10 +25,17 @@ def push(branch='master', message='add new features'):
 		local("git push origin {} ".format(branch))
 
 def prepare_deploy():
-	remote_path = '/opt/task_admin/'
+	remote_path = '~/projects/task_admin/project/task_admin'
 	with cd(remote_path):
-		sudo('git checkout master')
-		sudo('git pull origin master')
+		run('git checkout master')
+		run('git pull origin master')
 
+def deploy():
+	"""
+	É necessario adicionar as configurações da virtualenvwrapper no arquivo ".profile" e não
+	no arquivo ".bashrc" pois a sessão ssh carrega as variaveis de contexto do ".profile" e não 
+	do ".bashrc"
+	"""
+	run('workon task_admin')
 	
-		
+	
