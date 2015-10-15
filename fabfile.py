478afb2bad545469a793792dd8672abc1963f5b6
Rafael <rafa.cassau@gmail.com>
2015-10-15 13:03:53 -0300

    Pep 8 fix

--- fabfile.py
+++ fabfile.py
@@ -1,39 +1,36 @@
-#encoding: utf-8
+# encoding: utf-8
 
-from fabric.api import local, cd, run, env, prefix
-import os
+from fabric.api import local, cd, run, env
 
 env.hosts = ['ec2-52-89-60-124.us-west-2.compute.amazonaws.com']
 env.user = 'ubuntu'
 env.key_filename = '~/rafaelcassau.pem'
 
+
 def runserver():
-	local("./manage.py runserver 8000")
+    local("./manage.py runserver 8000")
+
 
 def execperm():
-	local("sudo chmod +x manage.py")
+    local("sudo chmod +x manage.py")
+
 
 def migrate():
-	local("./manage.py makemigrations")
-	local("./manage.py migrate")
+    local("./manage.py makemigrations")
+    local("./manage.py migrate")
 
-def push(branch='master', message='add new features'):
-	local_path = os.getcwd()
-	with cd(local_path):
-		local('git add .')
-		local('git commit -m "{}"'.format(message))
-		local("git push origin {} ".format(branch))
 
 def prepare_deploy():
-	remote_path = '~/projects/task_admin/project/task_admin'
-	with cd(remote_path):
-		run('git checkout master')
-		run('git pull origin master')
+    remote_path = '~/projects/task_admin/project/task_admin'
+    with cd(remote_path):
+        run('git checkout master')
+        run('git pull origin master')
+
 
 def deploy():
-	"""
-	É necessario adicionar as configurações da virtualenvwrapper no arquivo ".profile" e não
-	no arquivo ".bashrc" pois a sessão ssh carrega as variaveis de contexto do ".profile" e não 
-	do ".bashrc"
-	"""
-	run('workon task_admin')
+    """
+        É necessario adicionar as configurações da virtualenvwrapper no arquivo ".profile" e não
+        no arquivo ".bashrc" pois a sessão ssh carrega as variaveis de contexto do ".profile" e não
+        do ".bashrc"
+    """
+    run('workon task_admin')
--- tasks/api.py
+++ tasks/api.py
@@ -3,51 +3,47 @@ from restless.preparers import FieldsPreparer
 from tasks.models import Task
 
 
-
 class TaskResource(DjangoResource):
 
-	preparer = FieldsPreparer(fields={
-		'id': 'id',
-		'title': 'title',
-		'description': 'description',
-		'posted_on': 'posted_on'
-	})
+    preparer = FieldsPreparer(fields={
+        'id': 'id',
+        'title': 'title',
+        'description': 'description',
+        'posted_on': 'posted_on'
+    })
 
-	def is_authenticated(self):
-		return True
+    def is_authenticated(self):
+        return True
 
-	# GET /api/tasks/
-	def list(self):
-		return Task.objects.all()
+    # GET /api/tasks/
+    def list(self):
+        return Task.objects.all()
 
-	# GET /api/tasks/<pk>/
-	def detail(self, pk):
-		return Task.objects.get(pk=pk)
+    # GET /api/tasks/<pk>/
+    def detail(self, pk):
+        return Task.objects.get(pk=pk)
 
-	# POST /api/tasks/
-	def create(self):
-		return Task.objects.create(
-			title=self.data['title'],
-			description=self.data['description'],
-		)
+    # POST /api/tasks/
+    def create(self):
+        return Task.objects.create(
+            title=self.data['title'],
+            description=self.data['description'],
+        )
 
-	# PUT /api/task/<pk>/
-	def update(self, pk):
-		try:
-			task = Task.objects.get(pk=pk)
-		except Task.DoesNotExist:
-			task = Task()
+    # PUT /api/task/<pk>/
+    def update(self, pk):
+        try:
+            task = Task.objects.get(pk=pk)
+        except Task.DoesNotExist:
+            task = Task()
 
-		task.title = self.data['title']
-		task.description = self.data['description']
-		task.save()
+        task.title = self.data['title']
+        task.description = self.data['description']
+        task.save()
 
-		return task
+        return task
 
-	# DELETE /api/tasks/<pk>/
-	def delete(self, pk):
-		task = Task.objects.get(pk=pk).delete()
-		return task
-
-
-
+    # DELETE /api/tasks/<pk>/
+    def delete(self, pk):
+        task = Task.objects.get(pk=pk).delete()
+        return task
