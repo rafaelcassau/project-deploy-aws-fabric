382d835c406f83d4b7118317f1bfef0abaad5813
Rafael <rafa.cassau@gmail.com>
2015-10-19 15:17:50 -0200

    Fix ALLOWED_HOSTS in production.py

--- task_admin/settings/production.py
+++ task_admin/settings/production.py
@@ -2,6 +2,8 @@ from base import *
 
 DEBUG = False
 
+ALLOWED_HOSTS = ['ec2-52-89-60-124.us-west-2.compute.amazonaws.com']
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
